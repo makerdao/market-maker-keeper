d804862fe8151995672195c1f7ff48ebd1da6662
Mikael Brockman <mikael@brockman.se>
2017-09-09 09:02:47 +0300

    add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name="keeper",
+  version="0.5",
+  description="Maker Keeper framework",
+  author="reverendus",
+  url="https://github.com/makerdao/keeper",
+  packages=["keeper"]
+)
