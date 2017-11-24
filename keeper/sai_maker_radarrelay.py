2c0cb2e3b529bfa7d9b48ab5fbcfbf9b20d28a06
reverendus <reverendus@invalid>
2017-11-24 03:52:53 +0100

    Some test code for 0x/RadarRelay integration

--- keeper/sai_maker_radarrelay.py
+++ keeper/sai_maker_radarrelay.py
@@ -19,7 +19,7 @@ import argparse
 import sys
 
 from keeper import ERC20Token, Wad
-from keeper.api import Address
+from keeper.api import Address, synchronize
 from keeper.api.approval import directly
 from keeper.api.radarrelay import RadarRelay, RadarRelayApi, Order
 from keeper.sai import SaiKeeper
@@ -47,22 +47,42 @@ class SaiMakerRadarRelay(SaiKeeper):
         self.approve()
 
     def shutdown(self):
-        order = Order(maker=self.our_address,
-                      taker=Address("0x0000000000000000000000000000000000000000"),
-                      maker_token_address=Address("0x323b5d4c32345ced77393b3530b1eed0f346429d"),
-                      taker_token_address=Address("0xef7fff64389b814a946f3e92105513705ca6b990"),
-                      maker_token_amount=Wad(10000000000000000),
-                      taker_token_amount=Wad(20000000000000000),
-                      expiration_unix_timestamp_sec=42,
-                      salt=67006738228878699843088602623665307406148487219438534730168799356281242528500,
-                      exchange_contract_address=self.radar_relay.address,
-                      maker_fee=Wad(0),
-                      taker_fee=Wad(0),
-                      fee_recipient=Address("0x0000000000000000000000000000000000000000"))
+        order = self.radar_relay.create_order(Wad.from_number(1),
+                                              Wad.from_number(0.0030),
+                                              self.sai.address,
+                                              self.ether_token.address,
+                                              1511496715)
 
-        print(repr(self.radar_relay_api.calculate_fees(order)))
-        print("---")
-        print(self.radar_relay_api.get_orders_by_maker(self.our_address))
+        order_with_fees = self.radar_relay_api.calculate_fees(order)
+        signed_order = self.radar_relay.sign_order(order_with_fees)
+
+        print(self.radar_relay_api.submit_order(signed_order))
+
+        orders = self.radar_relay_api.get_orders_by_maker(self.our_address)
+        orders = list(filter(lambda order: self.radar_relay.get_unavailable_taker_token_amount(order) < order.taker_token_amount, orders))
+        synchronize(list(map(lambda order: self.radar_relay.cancel_order(order).transact_async(), orders)))
+
+
+
+
+        # print(repr(signed_order))
+
+        # order = Order(maker=self.our_address,
+        #               taker=Address("0x0000000000000000000000000000000000000000"),
+        #               maker_token_address=Address("0x323b5d4c32345ced77393b3530b1eed0f346429d"),
+        #               taker_token_address=Address("0xef7fff64389b814a946f3e92105513705ca6b990"),
+        #               maker_token_amount=Wad(10000000000000000),
+        #               taker_token_amount=Wad(20000000000000000),
+        #               expiration_unix_timestamp_sec=42,
+        #               salt=67006738228878699843088602623665307406148487219438534730168799356281242528500,
+        #               exchange_contract_address=self.radar_relay.address,
+        #               maker_fee=Wad(0),
+        #               taker_fee=Wad(0),
+        #               fee_recipient=Address("0x0000000000000000000000000000000000000000"))
+        #
+        # print(repr(self.radar_relay_api.calculate_fees(order)))
+        # print("---")
+        # print(self.radar_relay_api.get_orders_by_maker(self.our_address))
 
     def approve(self):
         """Approve 0x to access our 0x-WETH and SAI, so we can sell it on the exchange."""
