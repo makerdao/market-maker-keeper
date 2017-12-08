e82ac542f4a9b2b24490d71d66d73c9cc6131ab9
reverendus <reverendus@invalid>
2017-12-08 15:53:24 +0100

    Project name updated in `setup.py`

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(
-  name="keeper",
-  version="0.5",
-  description="Maker Keeper framework",
+  name="market-maker-keeper",
+  version="0.1",
+  description="Maker Keeper Framework: market-maker-keeper",
   author="reverendus",
-  url="https://github.com/makerdao/keeper",
-  packages=["keeper"]
+  url="https://github.com/makerdao/market-maker-keeper",
+  packages=["market-maker-keeper"]
 )
