2e2fd90e7751d441561d6a3e9cfc96caf9dfa753
reverendus <reverendus@invalid>
2017-12-25 16:03:44 +0100

    Implementing the Bibox API - `userInfo`

--- market_maker_keeper/bibox_api.py
+++ market_maker_keeper/bibox_api.py
@@ -17,8 +17,43 @@
 
 import hashlib
 import hmac
+import json
 
-dig = hmac.new(key=bytearray('7b58254791ada6c0194e6341953f862aff9a91b5', encoding='utf-8'),
-               msg='[{"cmd":"user/userInfo","body":{}}]'.encode('utf-8'),
-               digestmod=hashlib.md5)
-print(dig.hexdigest())
+import requests
+
+
+class BiboxApi:
+    def __init__(self, api_path: str, api_key: str, secret: str):
+        assert(isinstance(api_path, str))
+        assert(isinstance(api_key, str))
+        assert(isinstance(secret, str))
+
+        self.api_path = api_path
+        self.api_key = api_key
+        self.secret = secret
+
+    def _request(self, path: str, cmd: dict) -> dict:
+        assert(isinstance(path, str))
+        assert(isinstance(cmd, dict))
+
+        cmds = json.dumps([cmd])
+        call = {
+            "cmds": cmds,
+            "apikey": self.api_key,
+            "sign": self._sign(cmds)
+        }
+
+        result = requests.post(self.api_path + path, json=call, headers={"Content-Type": "application/json"})
+        result_json = result.json()
+
+        if 'error' in result_json:
+            raise Exception(f"API error, code {result_json['error']['code']}, msg: '{result_json['error']['msg']}'")
+
+        return result_json['result'][0]['result']
+
+    def _sign(self, msg: str) -> str:
+        assert(isinstance(msg, str))
+        return hmac.new(key=self.secret.encode('utf-8'), msg=msg.encode('utf-8'), digestmod=hashlib.md5).hexdigest()
+
+    def user_info(self) -> dict:
+        return self._request('/v1/user', {"cmd":"user/userInfo","body":{}})
