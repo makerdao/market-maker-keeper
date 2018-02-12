c3d9396280c08cd4eafcfc52cd53265936d0a699
reverendus <reverendus@invalid>
2018-02-12 17:33:34 +0100

    Old `--cov` argument removed from `test.sh`

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-PYTHONPATH=$PYTHONPATH:./lib/pymaker:./lib/pyexchange py.test --cov=keeper --cov=market_maker_keeper --cov-report=term --cov-append tests/
+PYTHONPATH=$PYTHONPATH:./lib/pymaker:./lib/pyexchange py.test --cov=market_maker_keeper --cov-report=term --cov-append tests/
