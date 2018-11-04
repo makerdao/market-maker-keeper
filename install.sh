348db6671a7784315bcb6dd27c88b85149c8a75c
reverendus <reverendus@invalid>
2018-11-04 12:56:40 +0100

    Predictable evaluation of `requirements.txt` files in `install.sh`

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ source _virtualenv/bin/activate
 # The advantage of using this method, in contrary to just calling `pip install -r requirements.txt` several times,
 # is that it can detect different versions of the same dependency and fail with a "Double requirement given"
 # error message.
-pip install $(cat requirements.txt $(find lib -name requirements.txt) | sort | uniq | sed 's/ *== */==/g')
+pip install $(cat requirements.txt $(find lib -name requirements.txt | sort) | sort | uniq | sed 's/ *== */==/g')
