9898b4c9c24b0e37682b1f6604df64bf7cb1b7be
@ianh <ith.harvey@gmail.com>
2019-05-23 09:16:27 -0700

    fixed Web3 url from https to http for localhost node

--- market_maker_keeper/airswap_market_maker_keeper.py
+++ market_maker_keeper/airswap_market_maker_keeper.py
@@ -137,7 +137,7 @@ class AirswapMarketMakerKeeper:
         self.arguments = parser.parse_args(args)
         setup_logging(self.arguments)
 
-        self.web3 = kwargs['web3'] if 'web3' in kwargs else Web3(HTTPProvider(endpoint_uri=f"https://{self.arguments.rpc_host}:{self.arguments.rpc_port}",
+        self.web3 = kwargs['web3'] if 'web3' in kwargs else Web3(HTTPProvider(endpoint_uri=f"http://{self.arguments.rpc_host}:{self.arguments.rpc_port}",
                                                                               request_kwargs={"timeout": self.arguments.rpc_timeout}))
         self.web3.eth.defaultAccount = self.arguments.eth_from
         self.our_address = Address(self.arguments.eth_from)
