9d62d3c181fca7dbdf78330268fbb67579479095
Mike <mikehathaway@makerdao.com>
2020-05-29 16:09:17 -0700

    account for dydx orders in all bands for available balance

--- market_maker_keeper/dydx_market_maker_keeper.py
+++ market_maker_keeper/dydx_market_maker_keeper.py
@@ -181,7 +181,7 @@ class DyDxMarketMakerKeeper(CEXKeeperAPI):
             orders = [order for order in our_buy_orders if band.includes(order, target_price.buy_price)]
             total_amount = total_buy_amount(orders)
             buy_limit_amount = bands.buy_limits.available_limit(time.time())
-            available_balance = our_buy_balance - total_amount
+            available_balance = our_buy_balance - total_in_buy_orders
             if total_amount < band.min_amount:
                 pay_amount = Wad.min(band.avg_amount - total_amount, available_balance, buy_limit_amount)
                 if total_in_buy_orders + pay_amount >= available_balance:
@@ -191,7 +191,7 @@ class DyDxMarketMakerKeeper(CEXKeeperAPI):
             orders = [order for order in our_sell_orders if band.includes(order, target_price.sell_price)]
             total_amount = total_sell_amount(orders)
             sell_limit_amount = bands.sell_limits.available_limit(time.time())
-            available_balance = our_sell_balance - total_amount
+            available_balance = our_sell_balance - total_in_sell_orders
             if total_amount < band.min_amount:
                 pay_amount = Wad.min(band.avg_amount - total_amount, available_balance, sell_limit_amount)
                 if total_in_sell_orders + pay_amount >= available_balance:
