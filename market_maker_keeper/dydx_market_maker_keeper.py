1f0098f42ed7d6e53cb62a7502096bdfd8b0b3bd
Mike <mikehathaway@makerdao.com>
2020-05-29 10:42:58 -0700

    handle additional dydx margin edge cases

--- market_maker_keeper/dydx_market_maker_keeper.py
+++ market_maker_keeper/dydx_market_maker_keeper.py
@@ -169,20 +169,20 @@ class DyDxMarketMakerKeeper(CEXKeeperAPI):
         for band in bands.buy_bands:
             orders = [order for order in our_buy_orders if band.includes(order, target_price.buy_price)]
             total_amount = total_buy_amount(orders)
-            limit_amount = bands.buy_limits.available_limit(time.time())
+            buy_limit_amount = bands.buy_limits.available_limit(time.time())
             if total_amount < band.min_amount:
-                pay_amount = Wad.min(band.avg_amount - total_amount, our_buy_balance, limit_amount)
-                if total_in_buy_orders + pay_amount > our_buy_balance:
+                pay_amount = Wad.min(band.avg_amount - total_amount, our_buy_balance, buy_limit_amount)
+                if total_in_buy_orders + pay_amount >= our_buy_balance:
                     band.min_amount = Wad(0)
 
         # check that placing new sell orders won't require margin
         for band in bands.sell_bands:
             orders = [order for order in our_sell_orders if band.includes(order, target_price.sell_price)]
             total_amount = total_sell_amount(orders)
-            limit_amount = bands.sell_limits.available_limit(time.time())
+            sell_limit_amount = bands.sell_limits.available_limit(time.time())
             if total_amount < band.min_amount:
-                pay_amount = Wad.min(band.avg_amount - total_amount, our_sell_balance, limit_amount)
-                if total_in_sell_orders + pay_amount > our_sell_balance:
+                pay_amount = Wad.min(band.avg_amount - total_amount, our_sell_balance, sell_limit_amount)
+                if total_in_sell_orders + pay_amount >= our_sell_balance:
                     band.min_amount = Wad(0)
 
         # Place new orders
