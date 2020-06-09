ef11510d07441cd62e7d53c2f829f2d7213bd001
Mike <mikehathaway@makerdao.com>
2020-06-09 14:12:51 -0700

    determine if in nonprod by presence of newrelease in url

--- market_maker_keeper/erisx_market_maker_keeper.py
+++ market_maker_keeper/erisx_market_maker_keeper.py
@@ -310,16 +310,11 @@ class ErisXMarketMakerKeeper(CEXKeeperAPI):
         return self.arguments.pair.split('/')[1].upper()
 
     def our_available_balance(self, our_balances: dict, token: str) -> Wad:
-        """
-        Nonprod balances are returned with a T prefix.
-        In the event that testing is being conducted in nonprod,
-        uncomment this code.
-        """
-        # if token == 'ETH':
-        #     token = 'TETH'
-        #
-        # if token == 'BTC':
-        #     token = 'TBTC'
+        if 'newrelease' in self.arguments.erisx_clearing_url:
+            if token == 'ETH':
+                token = 'TETH'
+            if token == 'BTC':
+                token = 'TBTC'
 
         token_balances = list(filter(lambda asset: asset['asset_type'].upper() == token, our_balances))
         if token_balances:
