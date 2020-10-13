01c9519635cc9e5aa9a7eb8dddf9b4e592b7421f
Mike <mikehathaway@makerdao.com>
2020-10-13 15:38:39 -0600

    initial commit with update SmartGasPrice

--- market_maker_keeper/gas.py
+++ market_maker_keeper/gas.py
@@ -18,8 +18,28 @@
 from typing import Optional
 
 from ethgasstation_client import EthGasStation
-from pymaker.gas import GasPrice, IncreasingGasPrice, FixedGasPrice, DefaultGasPrice
+from pymaker.gas import GasPrice, IncreasingGasPrice, FixedGasPrice, DefaultGasPrice, NodeAwareGasPrice
+from pygasprice_client import EtherchainOrg, EthGasStation, POANetwork
+from web3 import Web3
 
+
+def add_gas_arguments(parser: argparse.ArgumentParser):
+    gas_group = parser.add_mutually_exclusive_group()
+    gas_group.add_argument("--ethgasstation-api-key", type=str, default=None, help="ethgasstation API key")
+    gas_group.add_argument('--etherchain-gas-price', dest='etherchain_gas', action='store_true',
+                           help="Use etherchain.org gas price")
+    gas_group.add_argument('--poanetwork-gas-price', dest='poanetwork_gas', action='store_true',
+                           help="Use POANetwork gas price")
+    gas_group.add_argument('--fixed-gas-price', type=float, default=None,
+                           help="Uses a fixed value (in Gwei) instead of an external API to determine initial gas")
+    parser.add_argument("--gas-replace-after", type=int, default=42,
+                        help="Replace pending transactions after this many seconds")
+    parser.add_argument("--gas-initial-multiplier", type=float, default=1.0,
+                        help="Adjusts the initial API-provided 'fast' gas price")
+    parser.add_argument("--gas-reactive-multiplier", type=float, default=1.424,
+                        help="Increases gas price when transactions haven't been mined after some time")
+    parser.add_argument("--gas-maximum", type=float, default=8000,
+                        help="Places an upper bound (in Gwei) on the amount of gas to use for a single TX")
 
 class SmartGasPrice(GasPrice):
     """Simple and smart gas price scenario.
@@ -31,8 +51,22 @@ class SmartGasPrice(GasPrice):
 
     GWEI = 1000000000
 
-    def __init__(self, api_key: None):
-        self.gas_station = EthGasStation(refresh_interval=60, expiry=600, api_key=api_key)
+    def __init__(self, web3: Web3):
+        # self.gas_station = EthGasStation(refresh_interval=60, expiry=600, api_key=api_key)
+        self.gas_station = None
+        self.fixed_gas = None
+        if arguments.ethgasstation_api_key:
+            self.gas_station = EthGasStation(refresh_interval=60, expiry=600, api_key=arguments.ethgasstation_api_key)
+        elif arguments.etherchain_gas:
+            self.gas_station = EtherchainOrg(refresh_interval=60, expiry=600)
+        elif arguments.poanetwork_gas:
+            self.gas_station = POANetwork(refresh_interval=60, expiry=600, alt_url=arguments.poanetwork_url)
+        elif arguments.fixed_gas_price:
+            self.fixed_gas = int(round(arguments.fixed_gas_price * arguments.gas_initial_multiplier * self.GWEI))
+        self.every_secs = arguments.gas_replace_after
+        self.initial_multiplier = arguments.gas_initial_multiplier
+        self.coefficient = arguments.gas_reactive_multiplier
+        self.max_price = arguments.gas_maximum
 
     def get_gas_price(self, time_elapsed: int) -> Optional[int]:
         fast_price = self.gas_station.fast_price()
