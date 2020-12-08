177f906b002114ad01c737b91f1ad0cad9bb95bd
Mike Hathaway <mahathaway93@gmail.com>
2020-12-07 19:03:09 -0500

    Update setup instructions and fix local install (#210)
    * update pyexchange
    
    * update local setup instructions
    
    * fix unrelated uniswap logging issue
    
    Co-authored-by: Mike <mikehathaway@makerdao.com>

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ rm -rf _virtualenv
 virtualenv _virtualenv
 source _virtualenv/bin/activate
 
+# Downgrade pip to avoid issues with new 20.3.0 dependency resolver's handling of conflicting dependencies
+pip install pip==20.2.4
+
 # The advantage of using this method, in contrary to just calling `pip install -r requirements.txt` several times,
 # is that it can detect different versions of the same dependency and fail with a "Double requirement given"
 # error message.
